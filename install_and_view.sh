d6122af97cc1258182e22512f6a76eae6042d14a
Pratim66 <pratimsenapati1101@gmail.com>
2025-03-05 12:53:08 +0530

    Removed old Bash script

--- install_and_view.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Installing required tools..."
-sudo apt update && sudo apt install -y magic netgen imagemagick
-
-echo "Setup complete!"
-echo "You can now explore the results in the 'examples' folder."
-echo "To view layouts, use: magic -d XR -T <layout_file>.mag"
